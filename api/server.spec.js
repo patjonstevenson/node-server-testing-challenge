1e1fd4c4efe04fb65399dc0898105dca3efca36a
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-14 13:11:15 -0800

    added two tests for server GET / to server.spec.js

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,1 +1,20 @@
-const
+const request = require('supertest');
+
+const server = require('./server');
+
+it('should set DB_ENV to testing', () => {
+    expect(process.env.DB_ENV).toBe("testing");
+});
+
+describe('server', () => {
+    describe('GET /', () => {
+        it('should return status 200', async () => {
+            const res = await request(server).get('/');
+            return expect(res.status).toBe(200);
+        });
+        it('should return JSON formatted response', async () => {
+            const res = await request(server).get('/');
+            return expect(res.type).toMatch(/json/i);
+        })
+    });
+});
--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const server = require('./api/server');
 
 const port = process.env.PORT || 4000;
 
-server.get('/', (req, res) => {
-    res.status(200).send("It's alive");
-});
-
 server.listen(port, () => {
     console.log(`\nListening on port ${port}...\n`);
 });
