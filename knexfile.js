e054f7a19c4452f23578ff4eb5887ebbf55dc9cc
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-14 11:06:10 -0800

    created database directory with db-config; initialized knex; installed dotenv; installed cross-env and nodemon as dev deps

---
+++ knexfile.js
@@ -0,0 +1,59 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/content.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    }
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/testing.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
